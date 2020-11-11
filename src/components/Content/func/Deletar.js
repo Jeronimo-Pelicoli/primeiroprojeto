9ec3be7ca3c2d91283cad65da1640a236d59a82d
jeronimo-peicoli <jeronimosama@gmail.com>
2020-11-11 18:54:42 -0300

    add function delete

--- src/components/Content/func/Deletar.js
+++ src/components/Content/func/Deletar.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import api from "../../../services/api";
 
 export default function Deletar() {
-    return (
-      <div><p>Deletar</p></div>
-    )
+  const [ id, setId ] = useState('');
+
+  function handleDeleteProject(e) {
+      e.preventDefault();
+
+      api.delete(`proj/${id}`,{});
+  }
+  return (
+      <div>
+          <p>Deletar</p>
+          <form onSubmit={handleDeleteProject}>
+              <label htmlFor="id">IdProject</label>
+              <input type="text" id="id" value={id} onChange={(e) => {setId(e.target.value)}} />
+              
+              <button typer="submit">Enviar</button>
+          </form>
+      </div>
+  )
 }
